7af83ebed8b5494c300fe10c4df44078c30f38dc
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2025-11-12 15:41:13 -0300

    Configurações para variáveis de ambiente

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
-from .config import Config
+from .config import ProductionConfig, DevelopmentConfig
 from .extensions import db, migrate
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+
+    env = os.getenv("FLASK_ENV", "development")
+
+    if env == "production":
+        app.config.from_object(ProductionConfig)
+    else:
+        app.config.from_object(DevelopmentConfig)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     from .routes import main_bp
     app.register_blueprint(main_bp)
+
+    print(f"[*] Flask iniciado no ambiente: {env}")
 
     return app
--- app/config.py
+++ app/config.py
@@ -4,9 +4,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "mysql+pymysql://usuario:senha@localhost/mercearia_db"
-    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "chave-secreta-padrao")
+
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY não configurada.")
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL_DEV"
+    )
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("DATABASE_URL não configurada para produção.")
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
