ff715aa126f2d5f2119f625a1d0c17a2044cc6e9
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2025-11-13 23:39:37 -0300

    Adiciona endpoints CRUD para categorias, incluindo métodos para criar, atualizar, deletar e buscar categorias por nome no serviço.

---
+++ app/services/categories_service.py
@@ -0,0 +1,42 @@
+from app.extensions import db
+from app.models.categories import Categories
+
+def get_categories():
+    return Categories.query.all()
+
+def create_category(name, status):
+    category = Categories(name=name, status=status)
+   
+    db.session.add(category)
+    db.session.commit()
+
+    return category
+
+def get_by_name(name):
+    category = Categories.query.filter_by(name = name).first()
+    return category
+
+def update_category(id, **data):
+
+    category = Categories.query.get(id)
+    if not category:
+        return {"error": "Category not found"}, 404
+    
+    if "name" in data:
+        category.name = data["name"]
+    
+    if "status" in data:
+        category.status = data["status"]
+
+    db.session.commit() 
+
+    return {"message": "Category updated successfully"}, 200
+
+def delete_category(id):
+    category = Categories.query.get(id)
+    if not category:
+        return {"error": "Category not found"}, 404
+    
+    db.session.delete(category)
+    db.session.commit()
+    return 200
