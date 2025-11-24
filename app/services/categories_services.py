d78ffabec71be326bf9e4ac8b807157621aa9127
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2025-11-24 16:37:15 -0300

    Refactor# 60: Refatoração nas rotas e serviço de busca e cadastro de categories

--- app/services/categories_services.py
+++ app/services/categories_services.py
@@ -1,30 +1,30 @@
 from app.extensions import db
 from app.models.categories import Categories
+from werkzeug.exceptions import NotFound
 
 def get_categories():
     return Categories.query.all()
 
-def create_category(name, status):
-    category = Categories(name=name, status=status)
+def create_category(data):
+    category = Categories(**data)
    
     db.session.add(category)
     db.session.commit()
 
     return category
 
-def get_by_name(name):
-    category = Categories.query.filter_by(name = name).first()
+def search_by_name(name):
+    category = Categories.query.filter(Categories.name.ilike(f"%{name}%")).all()
     if not category:
-        return {"error": "Category not found"}, 404
+        raise NotFound("Category not found")
     return category
 
 def update_category(id, data):
 
     try:
         category = Categories.query.get(id)
-        print(category)
         if not category:
-            return {"error": "Category not found"}, 404
+            raise NotFound("Category not found")
         
         if "name" in data:
             category.name = data["name"]
