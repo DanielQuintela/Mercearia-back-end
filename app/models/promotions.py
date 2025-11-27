3a2d85e677595e5242ed4fb305b2b0be47c819d1
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2025-11-27 14:27:57 -0300

    Feature #63: Criação do modelo

---
+++ app/models/promotions.py
@@ -0,0 +1,29 @@
+from app.extensions import db
+from sqlalchemy.sql import func
+
+class Promotions(db.Model):
+    __tablename__ = "promotions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    status = db.Column(db.Boolean, default=True)
+    starts_at = db.Column(db.DateTime, nullable=False)
+    ends_at = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+    updated_at = db.Column(
+        db.DateTime,
+        server_default=func.now(),
+        server_onupdate=func.now(),
+        nullable=False
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "status": self.status,
+            "starts_at": self.starts_at.isoformat() if self.starts_at else None,
+            "ends_at": self.ends_at.isoformat() if self.ends_at else None,
+            "created_at": self.created_at.isoformat() if self.created_at else None,
+            "updated_at": self.updated_at.isoformat() if self.updated_at else None
+        }
