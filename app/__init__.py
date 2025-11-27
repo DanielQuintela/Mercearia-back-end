cfb58978fb01985b7b1a9eef534a3f1af9347a71
Daniel Quintela Lima Correia <danielcorreiaborges9@hotmail.com>
2025-11-27 13:24:47 -0300

    Feature #33: Criação de endpoints para autenticação de usuário com JWT

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import ProductionConfig, DevelopmentConfig, middleware_activate
-from .extensions import db, migrate, ma
+from .extensions import db, migrate, ma, jwt
 from .routes import register_blueprints
 
 import os
@@ -17,12 +17,13 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-    
     ma.init_app(app)
 
     middleware_activate(app)
 
     register_blueprints(app)
+
+    jwt.init_app(app)
 
     print(f"[*] Flask iniciado no ambiente: {env}")
 
